3e5951d5e9ccfa6d7144439828359c8d634618ed
Jordan Diamond <jordan.l.diamond@gmail.com>
2012-10-24 20:35:34 +1300

    Update search.php

--- search.php
+++ search.php
@@ -1,6 +1,22 @@
+
+
+ /*(C) Media Cookie 2012 and Onwards All Rights Reserved
+	Any external use other then Github or moodsearch.net render
+	the user breaching copyright.
+
+    This website is not permitted for external use
+    on any other webdomain other than the sites listed above
+
+    This copyright MUST remain on the document under
+    Australian/New Zealand Law.
+
+   Coded by Jordan Diamond & Daniel Johnson*/
+   
+
+    
+
 <?php
  
-//get date
 $button = $_GET['submit'];
 $search = $_GET['search'];
  
