d050c66ba1dc57172041edf02ed021b24a6f9633
unknown <jordan.l.diamond@gmail.com>
2012-10-24 20:02:32 +1300

    This is the easiest part of the whole search engine

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
+ 
+	<head> 
+ 
+		<title>LB-Search &raquo;</title> 
+              
+
+            
+ 
+		<style type="text/css" media="screen"> 
+ 
+			
+ 
+body, a, td, th, input, textarea, select {
+	font-family: Arial;
+	font-size: 12px;
+     
+ 
+	color: #444;
+ 
+	text-decoration: none;
+ 
+}
+ 
+a {
+ 
+	font-weight: bold;
+ 
+}
+ 
+img {
+	
+	border: 0;
+	
+}
+ 
+form {
+	
+	padding: 0;
+	margin: 0;
+	
+}
+ 
+.box {
+ 
+	background: #fcfcfc;
+	padding: 5px;
+	margin-bottom: 10px;
+ 
+}
+ 
+.box .content {
+ 
+	padding: 0px 5px 5px 5px;
+ 
+}
+ 
+big {
+ 
+	padding-bottom: 5px;
+ 
+	display: block;
+	color: #8899a1;
+ 
+	font-size: 24px;
+	font-weight: bold;
+ 
+}
+ 
+.menuitems {
+ 
+	margin-top: 5px;
+ 
+}
+ 
+.menuitems .a, .row.a, .menuitems .b, .row.b {
+ 
+	padding: 3px;
+	
+	display: block;
+	
+	font-weight: normal;
+ 
+}
+ 
+.row {
+	padding: 5px;
+}
+ 
+.menuitems .a, .row.a {
+ 
+	background-color: #f3f3f3;
+ 
+}
+ 
+.menuitems .b, .row.b {
+	
+	background-color: #f8f8f8;
+	
+}
+ 
+.square {
+ 
+	padding: 5px;
+	
+	margin-bottom: 5px;
+ 
+}
+ 
+.square strong {
+ 
+	font-size: 14px;
+ 
+}
+ 
+.square.menu {
+ 
+	color: #fff;
+	
+	margin-bottom: 0px;
+	
+	cursor: pointer;
+ 
+}
+ 
+.square.title {
+ 
+	background-color: #eee;
+	color: #444;
+ 
+}
+ 
+.square.good, .help.good {
+ 
+	background-color: #d9ffcf;
+	border-color: #ade5a3;
+	color: #1b801b;
+ 
+}
+ 
+.square.bad, .help.bad {
+ 
+	background-color: #ffcfcf;
+	border-color: #e5a3a3;
+	color: #801b1b;
+ 
+}
+ 
+.help {
+ 
+	padding: 5px;
+ 
+	font-size: 12px;
+ 
+	height: 16px;
+ 
+	position: relative;
+	left: -9px;
+ 
+	border: 1px #ccc solid;
+	border-bottom-width: 2px;
+ 
+	-moz-border-radius-topright: 3px;
+	-webkit-border-radius-topright: 3px;
+	border-radius-topright: 3px;
+ 
+	-moz-border-radius-bottomright: 3px;
+	-webkit-border-radius-bottomright: 3px;
+	border-radius-bottomright: 3px;
+}
+ 
+.help.grey {
+ 
+	background-color: #eee;
+}
+ 
+input, textarea, select {
+ 
+	border: 1px #e0e0e0 solid;
+	border-bottom-width: 2px;
+ 
+	-moz-border-radius: 3px;
+	-webkit-border-radius: 3px;
+	border-radius: 3px;
+ 
+	padding: 5px;
+ 
+	font-size: 12px;
+	font-weight: bold;
+ 
+}
+ 
+input:focus {
+ 
+	border-color: #ccc;
+ 
+	background-color: #fafafa;
+ 
+}
+ 
+input[type=submit], input.button {
+ 
+	color: #888;
+ 
+	border-color: #ddd;
+ 
+	background: #f0f0f0;
+ 
+	cursor: pointer;
+ 
+	padding: 3px;
+ 
+}
+ 
+input[type=submit]:hover, input.button:hover {
+ 
+	background-color: #e6e6e6;
+	border-color: #d6d6d6;
+ 
+}
+ 
+input[type=submit].green, input.button.green {
+	background-color: #d9ffcf;
+	border-color: #ade5a3;
+	color: #1b801b;			
+}
+ 
+input[type=submit].red, input.button.red {
+	background-color: #ffcfcf;
+	border-color: #e5a3a3;
+	color: #801b1b;		
+}
+ 
+td.label {
+ 
+	width: 25%;
+ 
+	text-align: right;
+ 
+}
+ 
+td.field {
+ 
+	width: 200px;
+ 
+	text-align: right;
+ 
+		</style> 
+ 
+	</head> 
+ 
+	<body> 
+  
+ 
+ <form action='search.php' method='GET'> 
+           
+ 
+       <center> 
+          <br />
+         <br />
+         <br />
+         <br />
+ <br />
+         <br />
+         <br />
+         <br />
+           <a href='http://lampstudios.net/search'/><img src='logo.png'/></a><br> 
+                   <input type='text' size='50' name='search'><input type='submit' name='submit' value='SEARCH'><br> 
+<br> 
+<a href="http://lampstudios.net" target="_blank">Lamp Studios</a> - <a href="http://www.sodopefm.com" target="_blank">So Dope FM</a> - <a href="http://namecheap.com" target="_blank">Namecheap</a><br> 
+&copy Site Coded By Jordan Diamond & Daniel Johnson</center> 
+ 
+ 
+ 
+ 
+</body> 
+  </html> 
---
+++ search.php
@@ -0,0 +1,81 @@
+<?php
+ 
+//get date
+$button = $_GET['submit'];
+$search = $_GET['search'];
+ 
+if (!$button)
+   echo "Please fill out the form";
+else
+{
+    if (strlen($search)<=2)
+    echo "The item you searched for was to small";
+    else
+    {
+     
+    echo "You searched for <b>$search</b> <hr size='1'>";
+    echo"<title>LB-Search &raquo; $search</title>";
+    echo"<style type='text/css'> 
+  @import url('style.css');
+ </style> ";
+    echo "<form action='search.php' method='GET'> 
+           <center> <a href="moodsearch.com/search'/><img src='logo.png'/></a><br>
+                    <input type='text' size='50' name='search'><input type='submit' name='submit' value='Search'><br>";
+     
+     //connect to database
+     
+     mysql_connect('localhost','','');
+     mysql_select_db('');
+     
+ 
+           //explode search term
+           $search_exploded = explode(" ",$search);
+           foreach($search_exploded as  $search_each)
+           {
+            //construct query
+            
+            $x++;
+            if ($x==1)
+               $construct .= "keywords LIKE '%$search_each%'";
+            else
+               $construct .= " OR keywords LIKE '%$search_each%'";
+ 
+           }
+ 
+     
+     //echo out construct
+     
+     $construct = "SELECT * FROM search WHERE $construct";
+     $run = mysql_query($construct);
+     
+     $foundnum = mysql_num_rows($run) or die(mysql_error());
+     
+     if ($foundnum==0)
+        echo "How about you try a non-retarded search, huh?";
+     else
+     {
+       
+       echo "$foundnum results found.<p><hr size='1'>";
+       
+       while ($runrows = mysql_fetch_assoc($run))
+       {
+        //get data
+        $title = $runrows['title']; 
+        $desc = $runrows['description'];
+        $url = $runrows['url'];
+        
+        echo "<b>$title</b><br>
+       $desc<br>
+       <a href='$url'>$url</a><p>";
+ 
+       }
+       
+     }
+     
+ 
+ 
+    }
+}
+ 
+ 
+?>
