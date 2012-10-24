7887bfc9252746c05e15fd250ce6d560d2975284
Jordan Diamond <jordan.l.diamond@gmail.com>
2012-10-24 20:33:43 +1300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!--
+
+	(C) Media Cookie 2012 and Onwards All Rights Reserved
+	Any external use other then Github or moodsearch.net render
+	the user breaching copyright.
+
+    This website is not permitted for external use
+    on any other webdomain other than the sites listed above
+
+    This copyright MUST remain on the document under
+    Australian/New Zealand Law.
+
+   Coded by Jordan Diamond & Daniel Johnson
+   
+
+    -->
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
